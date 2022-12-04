782fe588426b6e94b5e24d2364e5403ba38bb004
L140-beep <kartoshka0302@mail.ru>
2022-12-04 20:16:47 +0800

    simple toDo

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,49 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  let id = 0  
+
+  export default{
+    
+    
+    data(){
+      return {
+        newToDo : '',
+        todolist: [
+          {id: id++, text: 'Learn Vue'},
+          {id: id++, text: 'Learn Vue1'}
+        ]
+      }
+    },
+
+    methods: {
+      addToDo(){
+        this.todolist.push({id: id++, text: this.newToDo});
+        this.newToDo = '';
+      },
+      
+      removeToDo(todo){
+        this.todolist = this.todolist.filter(task => task != todo);
+      }
+
+    }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form @submit.prevent="addToDo">
+    <input v-model="newToDo">
+    <button type="submit"> Add toDo</button>
+  </form>
+
+  <ul>
+    <li v-for="todo in todolist">
+      {{todo.text}}
+      <button @click="removeToDo(todo)"> Remove </button>
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  .title{
+    color: red;
+  }
 </style>
