d02690674f199b446ca68fadd046e0028cc3a533
L140-beep <kartoshka0302@mail.ru>
2022-12-05 17:20:32 +0800

    added completed task and hide completed tasks button

--- src/App.vue
+++ src/App.vue
@@ -8,42 +8,72 @@
       return {
         newToDo : '',
         todolist: [
-          {id: id++, text: 'Learn Vue'},
-          {id: id++, text: 'Learn Vue1'}
-        ]
+          {id: id++, text: 'Learn Vue', done: false},
+          {id: id++, text: 'Learn Vue1', done: false}
+        ],
+        helper: "",
+        hideCompleted: false
       }
     },
 
     methods: {
       addToDo(){
-        this.todolist.push({id: id++, text: this.newToDo});
-        this.newToDo = '';
+        if (!this.newToDo){
+          this.helper = "Empty string!"
+        }
+        else{
+          this.helper = ""
+          this.todolist.push({id: id++, text: this.newToDo, done: false});
+          this.newToDo = '';
+        }
       },
       
       removeToDo(todo){
         this.todolist = this.todolist.filter(task => task != todo);
       }
+    },
 
+    computed: {
+      filtredToDos(){
+        if(this.hideCompleted){
+          return this.todolist.filter(todo => todo.done == false)
+        }
+        
+        return this.todolist
+      }
     }
   }
 </script>
 
 <template>
   <form @submit.prevent="addToDo">
-    <input v-model="newToDo">
-    <button type="submit"> Add toDo</button>
+    <input v-model="newToDo" placeholder="new toDo...">
+    <button type> Add toDo</button>
+    
+    <div v-if="helper">
+      {{helper}}
+    </div>
+
   </form>
 
-  <ul>
-    <li v-for="todo in todolist">
-      {{todo.text}}
-      <button @click="removeToDo(todo)"> Remove </button>
+   <ul>
+    <li v-for="todo in filtredToDos">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{done : todo.done}">{{todo.text}} </span>
+      <button @click="removeToDo(todo)"> X </button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{hideCompleted ? "Show all" : "Hide completed"}}
+  </button>
 </template>
 
 <style>
   .title{
     color: red;
+  }
+
+  .done{
+    text-decoration: line-through;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+//import Learn from './Learn.vue'
 
 createApp(App).mount('body')
-
+//createApp(Learn).mount('body')
 
